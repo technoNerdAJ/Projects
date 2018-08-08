b8173a2bcf55b16ebc567f8cf4a2e4f23eecc50c
Apoorv Jain <apoorvjain71@gmail.com>
2018-08-08 20:38:00 +0530

    Add files via upload

---
+++ Crear Monde Webpage Project/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crear Monde</title>
+	<link rel="stylesheet" type="text/css" href="css/web.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/fb.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<!-- Header -->
+<header id="main_head">
+<header id="inner_head">
+<section id="main_sec_bar">
+<section id="inner_sec_bar">
+    <div id="logo">
+    <img src="images/logo.jpg">
+    <p>Crear<span>Monde</span></p>
+    </div>
+	<nav id="nav">
+		<ul>
+			<li><a href="signup.php">Sign Up</a></li>
+			<li><a href="loginform.php">Login</a></li>
+		</ul>
+	</nav>
+</section>
+</section>
+
+<section id="slider">
+<div id="slider_content">
+	<img src="images/icon.png">
+	<p>Connect the world</p>
+</div>	
+</section>
+</header>
+</header>
+<!-- Header End -->
+
+<!-- Body Title -->
+<section id="main_body">
+	<section id="inner_body">
+	  <section id="box_sec">
+	      <div id="box">
+	      	<h4>Mortgage</h4>
+	      	<img src="images/icon1.png">
+	      	<p id="p">A little as 5% deposit can be saved to receive your new mortgage. Using mortgage calculators is quick and easy.</p>
+	      	<a href="#">MORE</a>
+	      </div>
+	      <div id="box">
+	      	<h4>Banking</h4>
+	      	<img src="images/icon2.png">
+	      	<p id="p">No matter what you need, a house, an apartment or an office – we have a crediting solution for you.</p>
+	      	<a href="#">MORE</a>
+	      </div>	
+	      <div id="box">
+	      	<h4>Office spaces</h4>
+	      	<img src="images/icon3.png">
+	      	<p id="p">We have to offer you various office spaces that can meet any client’s needs and wishes.</p>
+	      	<a href="#">MORE</a>
+	      </div>
+	      <div id="box">
+	      	<h4>Loans</h4>
+	      	<img src="images/icon4.png">
+	      	<p id="p">You will find plenty of variants that fit absolutely different requirements. Try out our service!</p>
+	      	<a href="#">MORE</a>
+	      </div>
+	  </section>
+	</section>
+</section>
+<!-- Body Title End -->
+
+<!-- Body Image -->
+<section id="main_body_image">
+	<section id="inner_body_image">
+	   <div id="over_content">
+	   	<img src="images/icon5.png">
+	   	<h1>Up-grade program</h1>
+	   	<p>You are dreaming about brilliant career, but it is too hard for you to climb the career ladder?</p> 
+        <p id="p1">So, company up-grade program is for you! With the help of the best lecturers and profs you will improve requisite skills.</p>
+        <a href="#">LEARN MORE<span><img src="images/arr.png"></span></a>
+	   </div>	
+	</section>
+</section>
+<!-- Body Image End -->
+
+<!-- Body Content1 -->
+<section id="main_body_content1">
+	<section id="inner_body_content1">
+		<section id="content1_box">
+			<h1>Welcome</h1>
+			<div id="underline"></div>
+			<div id="content1_image_main_box">
+				<div id="content1_image_inner_box1">
+					<img src="images/ny1.jpg">
+					<h4>Balancing risk and opportunity</h4>
+					<p>The stock market has historically returned 10% on average, but most investors have seen only a fraction of those returns. Why? Because of emotionally based investment decisions and attempts to time the market. At our company, we stress a more disciplined wealth management and financial planning process, and we back it up with a leading edge, integrated technology platform.</p>
+					<a href="#">MORE</a>
+				</div>
+				<div id="content1_image_inner_box2">
+					<img src="images/ny2.jpg">
+					<h4>Only award winning, best-in-class support</h4>
+					<p>We offer customers a truly unique set of services for large businesses. Because these services differ from our standard lineup of great products, they require a specialized customer support structure that blends local and national support for maximum efficiency and the best results. Our highly talented team’s advanced levels of training enable us to meet the demands of our customers 24/7.</p>
+				</div>
+			</div>
+		</section>
+	</section>
+</section>
+<!-- Body Content1 End -->
+
+<!-- Body Content2 -->
+<section id="main_body_content2">
+	<section id="inner_body_content2">
+		<section id="content2_box">
+			<div id="content2_image_main_box">
+				<h1>Leadership</h1>
+				<div id="underline1"></div>
+				<img src="images/image1.jpg">
+				<h4>We are the leaders</h4>
+				<p id="p2">Our company can boast the reputation of the trusted partner known worldwide. We are proud of the uncompromising quality of services we provide. Placing customers’ needs first in everything we do, we continue to grow rapidly as the industry leader.</p>
+				<a href="">MORE</a>
+			</div>
+			<div id="content2_image_main_box">
+				<h1>Research</h1>
+				<div id="underline1"></div>
+				<img src="images/image2.jpg">
+				<h4>Find an agent</h4>
+				<p id="p3">Our agents are professional in everything they do. Just call to our office or contact the agent you like via email or his/her social profile. You will get all the consultation you need. The agent can come to your office or house if you prefer personal meeting. You can be sure that he/she will do everything to meet all your requirements.</p>
+				<a href="">MORE</a>
+			</div>
+			<div id="content2_image_main_box">
+				<h1>Investment</h1>
+				<div id="underline1"></div>
+				<img src="images/image3.jpg">
+				<h4>Investment consulting</h4>
+				<p id="p4">Unlike other organizations that provide services of financial consulting, our company deals with a broader range of concerns. We are also recognized specialists in investment consulting, so if you would like to find out more about various aspects of investment, we will be glad to offer our help.</p>
+				<a href="">MORE</a>
+			</div>
+		</section>
+	</section>
+</section>
+<!-- Body Content2 End -->
+
+<!-- Map -->
+<section id="map">
+<div id="overlay" onClick="style.pointerEvents='none'"></div>
+<iframe
+  width="1519"
+  height="716"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAmiERQRgaEK1T-r_UBHdWgl7pvLZ2q3p4
+    &q=432 Park Avenue+New York+NY" allowfullscreen>
+</iframe>	
+</section>
+<!-- Map End -->
+
+<!-- Footer -->
+<footer id="main_footer">
+	<footer id="inner_footer"> 
+       <section id="footer_sec">
+       	   <div id="footer_sec_box">
+       	   	  <h4>Archives</h4>
+       	   	  <ul>
+       	   	  	<li><a href="#">May 2016</a></li>
+       	   	  	<li><a href="#">April 2016</a></li>
+       	   	  	<li><a href="#">March 2016</a></li>
+       	   	  	<li><a href="#">June 2016</a></li>
+       	   	  	<li><a href="#">July 2016</a></li>
+       	   	  	<li><a href="#">January 2016</a></li>
+       	   	  	<li><a href="#">December 2016</a></li>
+       	   	  </ul>
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Locations</h4>
+       	   	  <p id="p5">9870 St Vincent Place, Glasgow, DC 45 Fr 45.</p>
+       	   	  <p id="p6">9863 - 9867 Mill Road, Cambridge, MG09 99HT</p>
+       	   	  <p id="p7">8901 Marmora Road, Glasgow, D04 89GR.</p>
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Projects</h4>
+       	   	  <img id="img1" src="images/image4.jpg">
+       	   	  <img id="img2" src="images/image5.jpg">
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Contacts</h4>
+       	   	  <p id="p8">Call toll-free</p>
+       	   	  <p id="p9">+1 234 567 8900</p>
+       	   </div>
+       </section>
+       <section id="social_box">
+       	   <p>FINANCE COMPANY © 2017 • PRIVACY POLICY</p>
+       	   <ul>
+       	   	<li><div id="icon_back"><a class="icon-facebook" href="#"></a></div></li>
+       	   	<li><div id="icon_back"><a class="icon-gplus" href="#"></a></div></li>
+       	   	<li><a  class="icon-twitter" href="#"></a></li>
+       	   	<li><div id="icon_back2"><a class="icon-pinterest" href="#"></a></div></li>
+       	   	<li><div id="icon_back"><a class="icon-linkedin" href="#"></a></div></li>
+       	   	<li><a class="icon-instagram" href="#"></a></li>
+       	   </ul>
+       </section>
+	</footer>
+</footer>
+<!-- Footer End -->
+</body>
+</html>
---
+++ Crear Monde Webpage Project/signup.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crear Monde</title>
+	<link rel="stylesheet" type="text/css" href="css/web.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/fb.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<!-- Header -->
+<header id="main_sec_bar">
+<header id="inner_sec_bar">
+    <div id="logo">
+    <img src="images/logo.jpg">
+    <p>Crear<span>Monde</span></p>
+    </div>
+	<nav id="nav">
+		<ul>
+			<li><a  id="active" href="signup.php">Sign Up</a></li>
+			<li><a href="loginform.php">Login</a></li>
+		</ul>
+	</nav>
+</header>
+</header>
+<!-- Header End -->
+
+<!-- Signup Form -->
+<section id="main_body_content1">
+	<section id="inner_body_content1_2">
+		<section id="content1_box2">
+			<h1>Sign Up</h1>
+			<div id="underline3"></div>
+			<form id="form" role="form" method="post" action="#">
+			    <input type="text" name="username" placeholder="Username" autofocus>
+				<input type="text" name="email" placeholder="Email">
+				<input type="password" name="pass" placeholder="Password">
+				<input id="button" type="submit" value="Sign Up">
+			</form>
+		</section>
+	</section>
+</section>
+<!-- Signup Form End -->
+
+<!-- Footer -->
+<footer id="main_footer2">
+	<footer id="inner_footer2"> 
+       <section id="footer_sec">
+       	   <div id="footer_sec_box">
+       	   	  <h4>Archives</h4>
+       	   	  <ul>
+       	   	  	<li><a href="#">May 2016</a></li>
+       	   	  	<li><a href="#">April 2016</a></li>
+       	   	  	<li><a href="#">March 2016</a></li>
+       	   	  	<li><a href="#">June 2016</a></li>
+       	   	  	<li><a href="#">July 2016</a></li>
+       	   	  	<li><a href="#">January 2016</a></li>
+       	   	  	<li><a href="#">December 2016</a></li>
+       	   	  </ul>
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Locations</h4>
+       	   	  <p id="p5">9870 St Vincent Place, Glasgow, DC 45 Fr 45.</p>
+       	   	  <p id="p6">9863 - 9867 Mill Road, Cambridge, MG09 99HT</p>
+       	   	  <p id="p7">8901 Marmora Road, Glasgow, D04 89GR.</p>
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Projects</h4>
+       	   	  <img id="img1" src="images/image4.jpg">
+       	   	  <img id="img2" src="images/image5.jpg">
+       	   </div>
+       	   <div id="footer_sec_box">
+       	   	  <h4>Contacts</h4>
+       	   	  <p id="p8">Call toll-free</p>
+       	   	  <p id="p9">+1 234 567 8900</p>
+       	   </div>
+       </section>
+       <section id="social_box">
+       	   <p>FINANCE COMPANY © 2017 • PRIVACY POLICY</p>
+       	   <ul>
+       	   	<li><div id="icon_back"><a class="icon-facebook" href="#"></a></div></li>
+       	   	<li><div id="icon_back"><a class="icon-gplus" href="#"></a></div></li>
+       	   	<li><a  class="icon-twitter" href="#"></a></li>
+       	   	<li><div id="icon_back2"><a class="icon-pinterest" href="#"></a></div></li>
+       	   	<li><div id="icon_back"><a class="icon-linkedin" href="#"></a></div></li>
+       	   	<li><a class="icon-instagram" href="#"></a></li>
+       	   </ul>
+       </section>
+	</footer>
+</footer>
+<!-- Footer End -->
+</body>
+</html>
